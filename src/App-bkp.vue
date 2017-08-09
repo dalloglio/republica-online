2945b3309056677dc85effba6a197d44420bb557
dalloglio <ricardo.tech@live.com>
2017-08-09 14:42:22 -0300

    Botão de login do facebook.

--- src/App-bkp.vue
+++ src/App-bkp.vue
@@ -1,45 +1,66 @@
 <template>
-  <div id="app" :class="{
-    login: isLogin
-    }">
-    <app-header></app-header>
-    <main>
-      <transition name="fade">
-        <router-view></router-view>
-      </transition>
-    </main>
-    <app-footer></app-footer>
+  <div class="hello">
+    <div class="fb-login-button"
+      onlogin="me()"
+      data-scope="public_profile,email"
+      data-width="320"
+      data-max-rows="1"
+      data-size="small"
+      data-button-type="login_with"
+      data-show-faces="false"
+      data-auto-logout-link="false"
+      data-use-continue-as="false"></div>
+    <div id="status"></div>
   </div>
 </template>
 
 <script>
-import '@/assets/sass/app.scss'
-import appHeader from '@/components/Shared/Header.vue'
-import appFooter from '@/components/Shared/Footer.vue'
 export default {
-  name: 'app',
-  components: {
-    'app-header': appHeader,
-    'app-footer': appFooter
-  },
-  watch: {
-    '$route' (to, from) {
-      this.$store.dispatch('setPage', to.name)
+  name: 'hello',
+  data () {
+    return {
+      FB: null,
+      facebookReady: false
     }
   },
-  computed: {
-    isLogin () {
-      return this.$store.getters.appPageIs('auth.login')
+  beforeCreate () {
+    /* global window, FB, document */
+
+    (function (d, s, id) {
+      let js
+      let fjs = d.getElementsByTagName(s)[0]
+      if (d.getElementById(s)) {
+        return
+      }
+      js = d.createElement(s)
+      js.id = id
+      js.src = '//connect.facebook.net/pt_BR/sdk.js'
+      fjs.parentNode.insertBefore(js, fjs)
+    }(document, 'script', 'facebook-jssdk'))
+
+    let self = this
+    window.fbAsyncInit = function () {
+      FB.init({
+        appId: '252087528629349',
+        cookie: true,
+        xfbml: true,
+        version: 'v2.10'
+      })
+      FB.AppEvents.logPageView()
+      self.FB = FB
+      window.dispatchEvent(new Event('fb-sdk-ready'))
     }
   },
-  created () {
-    this.$store.dispatch('setPage', this.$route.name)
+  methods: {
+    onFacebookReady () {
+      this.facebookReady = true
+    }
+  },
+  mounted () {
+    window.addEventListener('fb-sdk-ready', this.onFacebookReady)
+  },
+  beforeDestroy () {
+    window.removeEventListener('fb-sdk-ready', this.onFacebookReady)
   }
 }
 </script>
-
-<style>
-#app.login {
-  background: transparent url('./assets/img/bg-login.jpg') no-repeat top center;
-}
-</style>
