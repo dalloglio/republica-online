d665721f23181023e9ffa11a60467209957be15e
Ricardo Pires <ricardo.tech@live.com>
2017-08-18 23:36:45 -0300

    Aplicando transition nas views.

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,9 @@
     }">
     <app-header></app-header>
     <main>
-      <router-view></router-view>
+      <transition name="fade">
+        <router-view></router-view>
+      </transition>
     </main>
     <app-footer></app-footer>
   </div>
@@ -45,5 +47,12 @@ export default {
 <style>
 #app.login {
   background: transparent url('./assets/img/bg-login.jpg') no-repeat top center;
+}
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .3s
+}
+.fade-enter, .fade-leave-to {
+  opacity: 0
 }
 </style>
