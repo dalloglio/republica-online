d422ce0d2dc0aff24f6dc997dac93a0b2f145072
Ricardo Pires Dall Oglio <ricardo.tech@live.com>
2018-01-12 06:58:59 -0200

    Npm install e variáveis de ambiente

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,6 +5,6 @@ module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   API_URL: '"http://localhost.api.republica.online"',
   API_CLIENT_ID: '"2"',
-  API_CLIENT_SECRET: '"gIwP6QmZ9Vf4lPQYaabJao2SyGqRUl7lZ2xZfCpi"',
+  API_CLIENT_SECRET: '"EyBZywHtvfKlN05vgxvDoIB3SPWRK6RA6T8yWv6I"',
   API_VERSION: '"v1"'
 })
